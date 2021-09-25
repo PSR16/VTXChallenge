9c2a3782d057de1c54fa4e34e0a2f22e4ad98909
Pegah <Prashidnia@gmail.com>
2021-09-24 23:45:46 -0400

    load database

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const http = require('http')
 const mongoose = require('mongoose')
+const Article = require('./models/articles');
 
-mongoose.connect('mongodb://articleDb:27017/test', { useNewUrlParser: true})
+mongoose.connect('mongodb://articleDb:27017/articles', { useNewUrlParser: true})
 
 var db = mongoose.connection;
+const articles = [
+  { type: 'pubmed', id: '7683628' },
+  { type: 'pubmed', id: '18456578' },
+  { type: 'pubmed', id: '20021716' },
+  { type: 'pubmed', id: '22658665' },
+  { type: 'pubmed', id: '22975760' },
+  { type: 'pubmed', id: '23891399' },
+  { type: 'pubmed', id: '23974870' },
+  { type: 'pubmed', id: '25087612' },
+  { type: 'pubmed', id: '27171515' },
+  { type: 'pubmed', id: '28546993' },
+];
 
 db.on('error', console.error.bind(console, 'Connection error'));
 db.once('open', function() {
   //connected
   console.log('Connected')
+  articles.map((id) => {
+    var article = new Article(id);
+    article.save(function (err, art){
+      if (err) return console.log(err);
+      console.log(art.id + " saved to DB")
+    })
+  });
 });
 
 const hostname = '0.0.0.0'
